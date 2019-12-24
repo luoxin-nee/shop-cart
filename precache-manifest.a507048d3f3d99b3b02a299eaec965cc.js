bfbd0a0fe0dd849a6db34adae56164acf84bec00
LinJuanmao <2211741172@qq.com>
2019-12-24 09:29:27 +0800

    Updates

---
+++ precache-manifest.a507048d3f3d99b3b02a299eaec965cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff901f618d5d643acbf6bd27b877b494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200901687fe2b30a88d1",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "b5cfe780ba6ef41d9aea",
+    "url": "/static/css/main.4a5b5cc3.chunk.css"
+  },
+  {
+    "revision": "200901687fe2b30a88d1",
+    "url": "/static/js/2.e89ee228.chunk.js"
+  },
+  {
+    "revision": "b5cfe780ba6ef41d9aea",
+    "url": "/static/js/main.3651b639.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
